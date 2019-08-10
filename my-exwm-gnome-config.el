be7985d60d75ccb13b391d500cf8540aec65d7ae
lesliebinbin <506120340@qq.com>
2019-08-11 02:06:30 +1000

    add the atomic chrome

--- my-exwm-gnome-config.el
+++ my-exwm-gnome-config.el
@@ -1,7 +1,10 @@
 (require 'exwm-randr)
 (setq exwm-randr-workspace-output-plist '(0 "LVDS-1" 1 "HDMI-1"))
+
+
 (add-hook 'exwm-randr-screen-change-hook
           (lambda ()
             (start-process-shell-command
-             "xrandr" nil "xrandr --output LVDS-1 --right-of HDMI-1 --auto --noprimary")))
+             "xrandr" nil "xrandr --output LVDS-1 --right-of HDMI-1 --auto --noprimary"))
+          )
 (exwm-randr-enable)
