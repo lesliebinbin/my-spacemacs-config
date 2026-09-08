5cdceeed2f6de9066a7c85af7ca47411af0a90f3
Leslie Huang <leslie.huang@basf.com>
2026-09-08 09:06:40 +0800

    update

--- emacs-config/layers.el
+++ emacs-config/layers.el
@@ -158,7 +158,7 @@ This function should only modify configuration layer settings."
    ;; Also include the dependencies as they will not be resolved automatically.
    dotspacemacs-additional-packages
    `(
-     (jupyter-eval :location (recipe :fetcher github-ssh :repo "lesliebinbin/emacs-jupyter-eval" :files ("*")))
+     (jupyter-eval :location (recipe :fetcher github-ssh :repo "lesliebinbin/emacs-jupyter-eval" :branch "feature/multi-kernel-renderer-access-modes" :files ("*")))
      (origami :location (recipe :fetcher github-ssh :repo "lesliebinbin/origami.el" :files ("*")))
      simple-httpd
      websocket
